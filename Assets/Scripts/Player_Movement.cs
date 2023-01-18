f98de1115f1b4f6538185f200a6377ec75e2d03c
Parisa Dabaghzade <khameneparisa@gmail.com>
2023-01-18 15:36:44 +0330

    continue the player and enemy movements

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -5,16 +5,62 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     public Transform transform;
+    public float speed = 1.5f;
+    public float rotationSpeed = 5f;
+    public  AudioSource CoinSound;
+    // public Score_Manager score_Value;
 
-    public float speed = 5f;
-
-    void Update() 
+    public GameObject gameOverPanel;
+    // Start is called before the first frame update
+    void Start()
     {
-        if(Input.GetKey(KeyCode.RightArrow)) {
+        // gameOverPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       Movment();
+       Clamp();
+
+
+    }
+
+    void Movment() {
+ if (Input.GetKey(KeyCode.RightArrow)) {
             transform.position += new Vector3(speed * Time.deltaTime ,0,0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,57), rotationSpeed * Time.deltaTime);
         }
-         if(Input.GetKey(KeyCode.LeftArrow)) {
+
+         if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.position -= new Vector3(speed * Time.deltaTime ,0,0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,115), rotationSpeed * Time.deltaTime);
         }
+
+        if (transform.rotation.z != 90) {
+             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,90), rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void Clamp() {
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x,-1.92f,1.92f);
+        transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Cars"){
+               Time.timeScale = 0 ;
+               gameOverPanel.SetActive(true);
+        }
+        if (collision.gameObject.tag == "Coin"){
+               CoinSound.Play();
+            //    score_Value.score += 10;
+               Destroy(collision.gameObject);
+        }
+        
     }
 }
