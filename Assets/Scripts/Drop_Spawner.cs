60ceb24721feff5769edf6e4f920466e21441195
Parisa Dabaghzade <khameneparisa@gmail.com>
2023-01-19 11:21:51 +0330

    change the gasoline sound and import the score part

--- Assets/Scripts/Drop_Spawner.cs
+++ Assets/Scripts/Drop_Spawner.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Drop_Spawner : MonoBehaviour
 {
+    public GameObject dropPrefabs;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(DropSpawner());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DropSpawn()
+    {
+        float rand = Random.Range(-1.87f,1.9f);
+        Instantiate(dropPrefabs, new Vector3(rand,transform.position.y,transform.position.z),Quaternion.identity);
+    }
+
+
+    IEnumerator DropSpawner() {
+        while (true) {
+            int time = Random.Range(8,20);
+            yield return new WaitForSeconds(time);
+            DropSpawn();
+        }
     }
 }
--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,10 +7,10 @@ public class Player_Movement : MonoBehaviour
     public Transform transform;
     public float speed = 1.5f;
     public float rotationSpeed = 5f;
-    public  AudioSource CoinSound;
-    // public Score_Manager score_Value;
-
+    public  AudioSource Gasoline_sound;
+    public Score_Manager score_Value;
     public GameObject gameOverPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,31 +21,27 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
        Movment();
        Clamp();
-
-
     }
 
     void Movment() {
- if (Input.GetKey(KeyCode.RightArrow)) {
+    if (Input.GetKey(KeyCode.RightArrow)) {
             transform.position += new Vector3(speed * Time.deltaTime ,0,0);
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,57), rotationSpeed * Time.deltaTime);
         }
 
          if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.position -= new Vector3(speed * Time.deltaTime ,0,0);
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,115), rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,115), rotationSpeed * Time.deltaTime);
         }
 
         if (transform.rotation.z != 90) {
-             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,90), rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,90), rotationSpeed * Time.deltaTime);
         }
     }
 
     void Clamp() {
-
         Vector3 pos = transform.position;
         pos.x = Mathf.Clamp(pos.x,-1.92f,1.92f);
         transform.position = pos;
@@ -56,9 +52,9 @@ public class Player_Movement : MonoBehaviour
                Time.timeScale = 0 ;
             //    gameOverPanel.SetActive(true);
         }
-        if (collision.gameObject.tag == "Coin"){
-               CoinSound.Play();
-            //    score_Value.score += 10;
+        if (collision.gameObject.tag == "Drop"){
+            //    Gasoline_sound.Play();
+               score_Value.score += 10;
                Destroy(collision.gameObject);
         }
         
